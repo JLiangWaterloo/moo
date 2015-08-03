e1a7ea9ed155e001c283a2f9b9222953807df4ba
JLiangWaterloo <paradoxical.reasoning@gmail.com>
2015-08-02 20:23:52 -0400

    Read MOO knapsack problems.

--- src/test/java/org/gsd/RandomKnapsack.java
+++ src/test/java/org/gsd/RandomKnapsack.java
@@ -1,12 +1,22 @@
 package org.gsd;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URISyntaxException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
-import org.chocosolver.solver.ResolutionPolicy;
+import java.util.List;
 import org.chocosolver.solver.Solver;
 import org.chocosolver.solver.SolverFactory;
 import org.chocosolver.solver.constraints.ICF;
+import org.chocosolver.solver.search.loop.monitors.IMonitorOpenNode;
 import org.chocosolver.solver.variables.IntVar;
-import org.chocosolver.solver.variables.VariableFactory;
+import org.chocosolver.solver.variables.VF;
 import org.junit.Test;
 
 /**
@@ -15,30 +25,151 @@ import org.junit.Test;
  */
 public class RandomKnapsack {
 
-    @Test
-    public void testKnapsack() {
-        for(int z = 0; z < 2; z++){
-        int n = 5;
-        for (int i = 2; i <= n; i++) {
-            for (int k = 0; k < 3;  k++) {
-                Solver solver = SolverFactory.makeSolver();
-                IntVar[] occurrences = VariableFactory.enumeratedArray("occurences", n, 0, 4, solver);
-                IntVar totalWeight = VariableFactory.enumerated("totalWeight", 0, 40, solver);
-                IntVar totalEnergy = VariableFactory.enumerated("totalEnergy", 0, 50, solver);
-                solver.post(ICF.knapsack(occurrences, totalWeight, totalEnergy, new int[]{1, 2, 3, 4, 5}, new int[]{2, 3, 4, 5, 7}));
+    private static List<Path> filesInDirectory(String resource) throws IOException, URISyntaxException {
+        List<Path> files = new ArrayList<>();
+        try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(RandomKnapsack.class.getResource(resource).toURI()))) {
+            directoryStream.forEach(files::add);
+        }
+        return files;
+    }
 
-                IntVar[] objectives = Arrays.copyOf(occurrences, i);
+    private static int[] parse1dArray(Reader in) throws IOException {
+        int c;
+        while (Character.isWhitespace(c = in.read())) {
+        }
+        if (c != '[') {
+            throw new IOException();
+        }
+        int[] array = new int[4];
+        int size = 0;
+
+        StringBuilder item = new StringBuilder();
+        while (true) {
+            c = in.read();
+            if (c == ',' || c == ']') {
+                if (size == array.length) {
+                    array = Arrays.copyOf(array, array.length * 2);
+                }
+                array[size++] = Integer.parseInt(item.toString());
+                if (c == ']') {
+                    return Arrays.copyOf(array, size);
+                }
+                item.setLength(0);
+            } else if (!Character.isWhitespace(c)) {
+                item.append((char) c);
+            }
+        }
+    }
+
+    private static int[][] parse2dArray(Reader in) throws IOException {
+        int c;
+        while (Character.isWhitespace(c = in.read())) {
+        }
+        if (c != '[') {
+            throw new IOException();
+        }
+        int[][] array = new int[4][];
+        int size = 0;
+
+        array[size++] = parse1dArray(in);
+        while ((c = in.read()) != ']') {
+            if (c == ',') {
+                if (size == array.length) {
+                    array = Arrays.copyOf(array, array.length * 2);
+                }
+                array[size++] = parse1dArray(in);
+            } else if (!Character.isWhitespace(c)) {
+                throw new Error("Unknown character: " + (char) c);
+            }
+        }
+        return Arrays.copyOf(array, size);
+    }
+
+    private static int sum(int... array) {
+        int sum = 0;
+        for (int i : array) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    private static class Pair {
+
+        IntVar[] totalEnergies;
+        int objects;
+
+        Pair(IntVar[] totalEnergies, int objects) {
+            this.totalEnergies = totalEnergies;
+            this.objects = objects;
+        }
+    }
+
+    private static Pair parseKnapsack(BufferedReader in, Solver solver) throws IOException {
+        int objectives = Integer.parseUnsignedInt(in.readLine());
+        int objects = Integer.parseUnsignedInt(in.readLine());
+        int capacity = Integer.parseUnsignedInt(in.readLine());
+
+        int[][] energies = parse2dArray(in);
+        if (energies.length != objectives) {
+            throw new IOException();
+        }
+        for (int[] energy : energies) {
+            if (energy.length != objects) {
+                throw new IOException();
+            }
+        }
+
+        int[] weights = parse1dArray(in);
+        if (weights.length != objects) {
+            throw new IOException();
+        }
+
+        IntVar[] occurences = VF.enumeratedArray("occurences", objects, 0, 50, solver);
+        IntVar totalWeight = VF.enumerated("totalWeight", 0, capacity, solver);
+        IntVar[] totalEnergies = new IntVar[energies.length];
+        for (int i = 0; i < energies.length; i++) {
+            totalEnergies[i] = VF.enumerated("totalEnergy[" + i + "]", 0, sum(energies[i]), solver);
+            solver.post(ICF.knapsack(occurences, totalWeight, totalEnergies[i], weights, energies[i]));
+        }
+        return new Pair(totalEnergies, objects);
+    }
+
+    @Test
+    public void testKnapsack() throws IOException, URISyntaxException {
+        for (Path path : filesInDirectory("/kp")) {
+            for (int k = 0; k < 3; k++) {
+                Solver solver = SolverFactory.makeSolver();
+
+                Pair pair = parseKnapsack(Files.newBufferedReader(path), solver);
+                IntVar[] objectives = pair.totalEnergies;
 
                 long start = System.currentTimeMillis();
+                solver.plugMonitor(new IMonitorOpenNode() {
+
+                    @Override
+                    public void beforeOpenNode() {
+                        if (System.currentTimeMillis() - start > 3000) {
+                            solver.getSearchLoop().interrupt("Times up", true);
+                        }
+                    }
+
+                    @Override
+                    public void afterOpenNode() {
+                    }
+                });
+
+                String out;
                 if (k == 0) {
-                    System.out.println("gia(" + i + "): " + Moo.gia(solver, objectives));
+                    out = "gia " + objectives.length + " " + pair.objects + " " + Moo.gia(solver, objectives);
                 } else if (k == 1) {
-                    System.out.println("classic(" + i + "): " + Moo.classic(solver, objectives).size());
+                    out = "classic " + objectives.length + " " + pair.objects + " " + Moo.classic(solver, objectives).size();
                 } else {
-                    System.out.println("oia(" + i + "): " + Moo.oia(solver, objectives).size());
+                    out = "oia " + objectives.length + " " + pair.objects + " " + Moo.oia(solver, objectives).size();
                 }
-                System.out.println(System.currentTimeMillis() - start);
+                boolean limit = solver.hasReachedLimit();
+                long time = System.currentTimeMillis() - start;
+                System.out.println(out + " " + time + " " + limit);
             }
-        }}
+        }
     }
 }
